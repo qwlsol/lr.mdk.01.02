52b6c1f6f11b9bf281eae4f52fefa21bd28ab40b
Daniil5903 <danilka7518@gmail.com>
2026-03-03 11:16:32 +0300

    продолжнаем тестовый проект пиццерии

---
+++ Pizzeria/PizzeriaClassLibrary/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaClassLibrary
+{
+    public class Pizza
+    {
+        private string name_;
+        private double price_;
+
+        public Pizza(string name, double price)
+        {
+            name_ = name;
+            price_ = price;
+        }
+        public string Name
+        {
+            get { return name_; }
+        }
+        public string Price
+        {
+            get { return price_.ToString(); }
+        }
+        public double GetPrice
+        {
+            get { return price_; }
+        }
+    }
+}
---
+++ Pizzeria/PizzeriaClassLibrary/PizzeriaService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaClassLibrary
+{
+    public class PizzeriaService
+    {
+        private Dictionary<string, List<Pizza>> pizza_ = new Dictionary<string, List<Pizza>>();
+        public PizzeriaService()
+        {
+        }
+        public bool AddPizzeria(string name, List<Pizza> menu)
+        {
+            if (name == null || name == "")
+            {
+                return false;
+            }
+            if (pizza_.ContainsKey(name))
+            {
+                return false;
+            }
+            pizza_.Add(name, menu);
+            return true;
+        }
+        public bool RemovePizzeria(string name)
+        {
+            if (name == null || name == "")
+            {
+                return false;
+            }
+            if (pizza_.ContainsKey(name))
+            {
+                return false;
+            }
+            pizza_.Remove(name);
+            return true;
+        }
+        public bool AddPizza(string pizzeriaName, Pizza pizza)
+        {
+            if (pizzeriaName == null || pizzeriaName == "")
+            {
+                return false;
+            }
+            if (pizza == null)
+            {
+                return false;
+            }
+            if (pizza_.ContainsKey(pizzeriaName))
+            {
+                return false;
+            }
+            pizza_[pizzeriaName].Add(pizza);
+            return true;
+        }
+        public bool RemovePizzaFromPizzeria(string pizzeriaName, string pizzaName)
+        {
+            if (pizzeriaName == null || pizzeriaName == "" || pizzaName == null || pizzaName == "")
+            {
+                return false;
+            }
+            if (pizza_.ContainsKey(pizzeriaName))
+            {
+                return false;
+            }
+            Pizza pizzaToRemove = null;
+            foreach (var pizza in pizza_[pizzeriaName]) 
+            {
+                if (pizza.Name.ToLower()== pizzaName.ToLower())
+                {
+                    pizzaToRemove = pizza;
+                    break;
+                }
+            }
+            if (pizzaToRemove != null) 
+            {
+                pizza_[pizzeriaName].Remove(pizzaToRemove);
+                return true;
+            }
+            return false;
+        }
+        public string FindPizza(string pizzaName)
+        {
+            if (pizzaName == null || pizzaName == "")
+            {
+                return "Вы не указали название пиццы";
+            }
+            string result = "\nПицца '" + pizzaName + "'найдена в:\n";
+        }
+    }
+}
